e39f8244829f0f716b0ea2df1e5d58d9e53e753c
Casey Hedrick <caseyhedrick@u.boisetate.edu>
2020-09-09 17:38:30 -0700

    MiniAppTest

---
+++ MiniApps/FirstViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FirstViewController.swift
+//  MiniApps
+//
+//  Created by user181417 on 9/3/20.
+//  Copyright © 2020 Boise State. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func concatTapped(_ sender: Any) {
+        guard firstNameTextField.text!.isEmpty else {
+            return
+        }
+        let concatenatedString =  "Hello \(firstNameTextField.text!) \(lastNameTextField.text!), nice to meet you!"
+//        print(firstNameTextField.text)
+        
+        outputLabel.text = concatenatedString
+        
+    }
+ //I make code change.
+}
+
---
+++ MiniApps/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MiniApps
+//
+//  Created by user181417 on 9/3/20.
+//  Copyright © 2020 Boise State. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
